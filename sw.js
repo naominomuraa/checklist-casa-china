dbcf38e7afb8df8c671bf6b646de0b5834bc6ef6
naominomuraa <156229644+naominomuraa@users.noreply.github.com>
2026-07-23 11:46:53 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE='cc2-v7';
+const ASSETS=[
+  '/checklist-casa-china/',
+  '/checklist-casa-china/index.html',
+  '/checklist-casa-china/manifest.json',
+  '/checklist-casa-china/icon-192.png',
+  '/checklist-casa-china/icon-512.png'
+];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).catch(()=>caches.match('/checklist-casa-china/'))));
+});
